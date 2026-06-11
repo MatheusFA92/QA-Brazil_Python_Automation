c64bb88afe2fc6f591194a9efa2fc4498800eb5c
Windows10 <matheusfragaalves@gmail.com>
2026-06-11 17:40:23 -0300

    Subindo o Projeto Sprint 7

--- data.py
+++ data.py
@@ -1,7 +1,15 @@
-URBAN_ROUTES_URL = 'https://cnt-cae6a2d6-4bc2-4c72-9919-3eea835ee8f4.containerhub.tripleten-services.com?lng=pt'
+URBAN_ROUTES_URL = 'https://cnt-c5a4e348-ec65-4f5e-9e68-6cd0ff16e442.containerhub.tripleten-services.com?lng=pt'
+
+# Arquivo com constantes
+
 ADDRESS_FROM = 'East 2nd Street, 601'
+
 ADDRESS_TO = '1300 1st St'
+
 PHONE_NUMBER = '+1 123 123 12 12'
+
 CARD_NUMBER = '1234 5678 9100'
+
 CARD_CODE = '1111'
+
 MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
--- main.py
+++ main.py
@@ -14,42 +14,45 @@ class TestUrbanRoutes:
             )
 
     def test_set_route(self):
-        # Adicionar em S8
-        print("função criada para definir a rota")
+        # Adicionar na Sprint S8
         pass
+        print("função criada para definir a rota")
 
     def test_select_plan(self):
-        # Adicionar em S8
-        print("função criada para selecionar o plano")
+        # Adicionar na Sprint S8
         pass
+        print("função criada para selecionar o plano")
 
     def test_fill_phone_number(self):
-        # Adicionar em S8
-        print("função criada para preencher o número de telefone")
+        # Adicionar na Sprint S8
         pass
+        print("função criada para preencher o número de telefone")
 
     def test_fill_card(self):
-        # Adicionar em S8
-        print("função criada para preencher os dados do cartão")
+        # Adicionar na Sprint S8
         pass
+        print("função criada para preencher os dados do cartão")
 
     def test_comment_for_driver(self):
-        # Adicionar em S8
-        print("função criada para adicionar comentário ao motorista")
+        # Adicionar na Sprint S8
         pass
+        print("função criada para adicionar comentário ao motorista")
 
     def test_order_blanket_and_handkerchiefs(self):
-        # Adicionar em S8
-        print("função criada para solicitar cobertor e lenços")
+        # Adicionar na Sprint S8
         pass
+        print("função criada para solicitar cobertor e guardanapos")
 
     def test_order_2_ice_creams(self):
-        # Adicionar em S8
-        print("função criada para solicitar 2 sorvetes")
+        numbers_of_ice_creams = 2
+
+        for count in range(numbers_of_ice_creams):
+            #Adicionar na Sprint S8
+            print("função criada para pedir quantidade de sorvetes")
         pass
 
     def test_car_search_model_appears(self):
-        # Adicionar em S8
+        # Adicionar na Sprint S8
         print("função criada para verificar o modelo do carro")
         pass
 

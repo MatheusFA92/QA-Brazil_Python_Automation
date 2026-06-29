aa686578f2e5473ffa238981d558ae2b27b6fe4b
Windows10 <matheusfragaalves@gmail.com>
2026-06-29 12:03:16 -0300

    test_set_route automate

--- main.py
+++ main.py
@@ -1,65 +1,75 @@
 import data
 import helpers
 
+from pages import UrbanRoutesPage
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
+        from selenium.webdriver import DesiredCapabilities
+        capabilities = DesiredCapabilities.CHROME
+        capabilities['goog:loggingPrefs'] = {'performance': 'ALL'}
+        cls.driver = Chrome()
+        cls.driver.implicitly_wait(5)
+
         if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
-            print("Conectado ao servidor Urban Routes")
+            print("Conectado ao servidor Urban Routes.")
         else:
-            print(
-                "Não foi possível conectar ao Urban Routes. "
-                "Verifique se o servidor está ligado e ainda em execução."
-            )
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+
+    def setup_method(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
+        self.page = UrbanRoutesPage(self.driver)
 
     def test_set_route(self):
-        # Adicionar na Sprint S8
-        pass
-        print("função criada para definir a rota")
+        self.page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        assert self.page.get_from_location(data.ADDRESS_FROM, data.ADDRESS_TO)
+        assert self.page.get_to_location() == data.ADDRESS_TO
+        time.sleep(10)
 
     def test_select_plan(self):
-        # Adicionar na Sprint S8
+        #Adicionar EM s8
+        print("funcão criada para definir o mapa")
         pass
-        print("função criada para selecionar o plano")
 
     def test_fill_phone_number(self):
-        # Adicionar na Sprint S8
+        #Adicionar em S8
+        print("funcão criada para definir o número de telefone")
         pass
-        print("função criada para preencher o número de telefone")
 
     def test_fill_card(self):
-        # Adicionar na Sprint S8
+        # Adicionar em S8
+        print("função criada para definir o cartão para pagamento")
         pass
-        print("função criada para preencher os dados do cartão")
 
     def test_comment_for_driver(self):
-        # Adicionar na Sprint S8
+        # Adicionar em S8
+        print("função criada para adicionar um comentário ao motorista")
         pass
-        print("função criada para adicionar comentário ao motorista")
 
     def test_order_blanket_and_handkerchiefs(self):
-        # Adicionar na Sprint S8
+        # Adicionar em S8
+        print("função criada para adicionar cobertor e lenços")
         pass
-        print("função criada para solicitar cobertor e guardanapos")
 
     def test_order_2_ice_creams(self):
-        numbers_of_ice_creams = 2
+        order_ice_cream = 2
+        for count in range(order_ice_cream):
+            # Adicionar em S8
+            print("função criada para pedir sorvete")
+            pass
 
-        for count in range(numbers_of_ice_creams):
-            #Adicionar na Sprint S8
-            print("função criada para pedir quantidade de sorvetes")
-        pass
+    def test_car_serarch_model_appears(self):
+            # Adicionar em S8
+            print("função criada para mostrar o modelo de carro encontrado")
+            pass
 
-    def test_car_search_model_appears(self):
-        # Adicionar na Sprint S8
-        print("função criada para verificar o modelo do carro")
-        pass
+    @classmethod
+    def teardown_class(cls):
+        cls.driver.quit()
 
-    def test_add_2_ice_creams(self):
-        number_of_ice_creams = 2
 
-        for count in range(number_of_ice_creams):
-            print("função criada para adicionar a quantidade de sorvetes")
-
-        pass
